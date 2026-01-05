985a4a93b9068de0451c5fb55b33ebe69d5047dc
Carlos Borges <carloos-boorges@hotmail.com>
2026-01-05 17:32:41 -0300

    Melhora atualização de cliente com validações e MapStruct

--- src/main/java/dev/borges/BarberTech/controller/ClienteController.java
+++ src/main/java/dev/borges/BarberTech/controller/ClienteController.java
@@ -1,11 +1,13 @@
 package dev.borges.BarberTech.controller;
 import dev.borges.BarberTech.dto.ClienteRequestDTO;
 import dev.borges.BarberTech.dto.ClienteResponseDTO;
+import dev.borges.BarberTech.entity.ClienteModel;
 import dev.borges.BarberTech.service.ClienteService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cliente")
@@ -18,7 +20,24 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ClienteResponseDTO adicionarCliente(@RequestBody ClienteRequestDTO novoCliente){
-        return clienteService.adicionarCliente(novoCliente);
+    public ResponseEntity<ClienteResponseDTO> adicionarCliente(@RequestBody ClienteRequestDTO novoCliente){
+        ClienteResponseDTO cliente = clienteService.adicionarCliente(novoCliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cliente);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteResponseDTO>> listarCliente(){
+        List<ClienteResponseDTO> lista = clienteService.listarClientes();
+
+        if  (lista.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(lista);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteResponseDTO> listarPorId(@PathVariable Long id){
+        ClienteResponseDTO cliente = clienteService.listarPorId(id);
+        return ResponseEntity.ok(cliente);
     }
 }
--- src/main/java/dev/borges/BarberTech/service/ClienteService.java
+++ src/main/java/dev/borges/BarberTech/service/ClienteService.java
@@ -5,7 +5,10 @@ import dev.borges.BarberTech.entity.ClienteModel;
 import dev.borges.BarberTech.mapper.ClienteMapper;
 import dev.borges.BarberTech.repository.ClienteRepository;
 import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClienteService {
@@ -20,14 +23,55 @@ public class ClienteService {
 
     public ClienteResponseDTO adicionarCliente(ClienteRequestDTO novoCliente){
 
-        if(clienteRepository.existsByCpf(novoCliente.cpf())){
+        if(clienteRepository.existsByCpf(novoCliente.getCpf())){
             throw new EntityExistsException("CPF já cadastrado");
         }
-        if(clienteRepository.existsByEmail(novoCliente.email())){
+        if(clienteRepository.existsByEmail(novoCliente.getEmail())){
             throw new EntityExistsException("Email já cadastrado");
         }
         ClienteModel clienteSalvo = clienteMapper.toModel(novoCliente);
         return clienteMapper.toResponse(clienteRepository.save(clienteSalvo));
 
     }
+
+    public List<ClienteResponseDTO> listarClientes(){
+        List<ClienteModel> cliente = clienteRepository.findAll();
+        return clienteRepository.findAll()
+                .stream()
+                .map(clienteMapper::toResponse)
+                .toList();
+    }
+
+    public ClienteResponseDTO listarPorId(Long id){
+        ClienteModel cliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Cliente não encontrado"));
+        return clienteMapper.toResponse(cliente);
+
+    }
+
+    public ClienteResponseDTO alterarCliente(ClienteRequestDTO request, Long id){
+        ClienteModel cliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Cliente com "+ id + "não encontrado."));
+
+        if (!cliente.getCpf().equals(request.getCpf())
+            && clienteRepository.existsByCpf(request.getCpf())){
+            throw new EntityExistsException("CPF já cadastrado.");
+        }
+
+        if (!cliente.getEmail().equals(request.getEmail())
+            && clienteRepository.existsByEmail(request.getEmail())){
+            throw new EntityExistsException("Cliente com email já cadastrado.");
+        }
+
+        clienteMapper.updateFromDto(request, cliente);
+
+        ClienteModel clienteSalvo = clienteRepository.save(cliente);
+
+        return clienteMapper.toResponse(clienteSalvo);
+
+
+
+    }
+
+
 }
