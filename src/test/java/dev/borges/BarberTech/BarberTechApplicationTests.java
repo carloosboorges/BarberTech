1b49cb222157b3ad7cb4ee84fe4d8e71d027b37d
carloosboorges <carloos-boorges@hotmail.com>
2026-01-19 18:12:30 -0300

    test: atualizar testes para novas funcionalidades

--- src/test/java/dev/borges/BarberTech/BarberTechApplicationTests.java
+++ src/test/java/dev/borges/BarberTech/BarberTechApplicationTests.java
@@ -2,8 +2,10 @@ package dev.borges.BarberTech;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BarberTechApplicationTests {
 
 	@Test
