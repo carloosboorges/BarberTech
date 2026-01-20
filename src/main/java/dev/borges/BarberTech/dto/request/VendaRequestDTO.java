cb0d2ad7ea8cd304b0e7b6e9ff9f24d5547db1ff
carloosboorges <carloos-boorges@hotmail.com>
2026-01-20 05:18:20 -0300

    refactor: remove imports não utilizados

--- src/main/java/dev/borges/BarberTech/dto/request/VendaRequestDTO.java
+++ src/main/java/dev/borges/BarberTech/dto/request/VendaRequestDTO.java
@@ -1,9 +1,8 @@
 package dev.borges.BarberTech.dto.request;
-import dev.borges.BarberTech.entity.ClienteModel;
-import dev.borges.BarberTech.entity.ItemVendaModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.List;
 
